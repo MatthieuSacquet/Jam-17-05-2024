1cd23138b5c19c8a9f467a286728aa7400aaba60
jasonzohou <jason.zohou@epitech.eu>
2024-05-18 17:38:57 +0200

    [ADD] GAME_MENU

---
+++ MENU_PRGRAME/JAM2/MENU/MAIN_MENU/menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://GAME/Level1.tscn")
+
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://MENU/SETTINGS_MENU/settings_menu.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
---
+++ MENU_PRGRAME/JAM2/MENU/SETTINGS_MENU/settings_menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_volume_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_to_menu_pressed():
+	get_tree().change_scene_to_file("res://MENU/MAIN_MENU/menu.tscn")
